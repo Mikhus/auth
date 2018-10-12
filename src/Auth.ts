abd70287831ab19caaa99414b0909abec96e43cb
Mykhailo Stadnyk <mikhus@gmail.com>
2018-10-12 08:50:35 +0300

    verify fixed

--- src/Auth.ts
+++ src/Auth.ts
@@ -85,7 +85,7 @@ export class Auth extends IMQService {
     public async verify(token: string): Promise<object | null> {
         const data = jwtDecode(token) as any;
 
-        if (!data || data && data.exp && new Date().getTime() > data.exp * 1000) {
+        if (!data || data.exp && new Date().getTime() > data.exp * 1000) {
             return null; // token expired
         }
 
